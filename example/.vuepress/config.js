4ddccff55947fc1ea89bcc0f10d32f4215583d7f
Okarin1 <Okarin1024@gmail.com>
2022-04-12 23:59:04 +0800

    feat: post

---
+++ example/.vuepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    title: '纯粹阅读',
+    description: '只是为了简单的阅读',
+    theme: "reading",
+    themeConfig: {
+      author:'Okarin',
+      authorLink: 'https://okarin.cn',
+    }
+  }
